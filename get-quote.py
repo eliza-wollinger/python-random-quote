df6ea6249457313b471f00ac930a137417780378
wollieliza <wollingereliza@gmail.com>
2021-06-27 01:53:23 -0300

    Trying to make my bot obey me...

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,6 @@
+from random import *
+
 def main():
-  # print("Keep it logically awesome.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
-
-if __name__== "__main__":
-  main()
+  with open('quotes.txt') as file:
+    for line in file:
+        print(random.choice(line))
